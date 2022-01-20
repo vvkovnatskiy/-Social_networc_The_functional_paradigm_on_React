e4a578c415f60d71e994a8ac0ba681f9e3bc1714
Ковнацкий Владислав Владимирович <kovnatskiyvv@msk.aa.loc>
2022-01-21 02:15:24 +0300

    lessen 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img className='logo-header' src='https://www.xnview.com/img/app-xnsoft-360.png' />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="img-container">
+        <img className="img-content" src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
+      </div>
+      <div>
+        <img className="logo-content" src='http://filestack.itcraftlab.com/wp-content/uploads/2018/05/Kitten-min.png' />
+      </div>
+      <div>
+        Posts
+        <div>
+          New post
+        </div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
