4cf38a015f0d194e3b1c3fd596f2ad5c7c4d9871
VVK <xxxxx@mail.xxx>
2022-02-03 02:36:35 +0300

    using props in Post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,23 +4,25 @@ import Post from './Post/Post';
 const MyPosts = () => {
   return (
     <div>
-      <div className={s.logo_container}>
-        <img className={s.logo} src='http://filestack.itcraftlab.com/wp-content/uploads/2018/05/Kitten-min.png' />
-      </div>
-      My Posts
-      <div>
-        <textarea></textarea>
-        <button>Add post</button>
-        <button>Remove</button>
+      <div className={s.myPost}>
+        <div className={s.logo_container}>
+          <img className={s.logo} src='http://filestack.itcraftlab.com/wp-content/uploads/2018/05/Kitten-min.png' />
+        </div>
+        My Posts
+        <div>
+          <textarea></textarea>
+          <button>Add post</button>
+          <button>Remove</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+        <Post nickName='Ivan' message='Hi, how are you?' nOfL='15'/>
+        <Post nickName='Huan' message='Hi, I am fun. And you?' nOfL='12'/>
+        <Post nickName='Sergio' message='...something' nOfL='9'/>
+        <Post nickName='Vladlen' message='...something' nOfL='23'/>
+        <Post nickName='Oleg' message='...something' nOfL='19'/>
+        <Post nickName='Jon' message='...something' nOfL='15'/>
+        <Post nickName='Jesus' message='...something' nOfL='15'/>
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,17 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
       <div className={s.item}>
-      <img src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'/>
-        post 1
-        <div><span>like</span></div>
+        <div className={s.privateInfo}>
+          <img src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'/>
+          <div>
+            <p>Nickname:</p> 
+            <p>{props.nickName}</p>
+          </div>
+        </div>
+        <span>{props.message}</span>
+        <div><button>Like</button><span>number of likes: {props.nOfL}</span></div>
       </div>
   );
 }
